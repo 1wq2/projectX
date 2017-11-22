94f0b606c85780e5e7a88fe9e476fb34f03a879b
Stanislau Khomich 550503 <stanislaukhomich@gmail.com>
2017-11-22 14:52:46 +0300

    model migration

--- webpages/models.py
+++ webpages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Template(models.Model):
+    template_logo = models.CharField(max_length=100)
+    template_title = models.CharField(max_length=100)
+
+#class Template_change(models.Model)
+    #template = models.ForeignKey(Template, on_delete=models.CASCADE)
