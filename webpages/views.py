3a6f90c21055cfd39d998eaa5f54955c2cdd73e9
Stanislau Khomich 550503 <stanislaukhomich@gmail.com>
2017-11-22 13:59:01 +0300

    urls and database migration

--- webpages/views.py
+++ webpages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1> Webpages page is currently in the development. Please be patient</h1>")
