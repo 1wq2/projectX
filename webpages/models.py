1d3cb92275e2ca3ae29e1408e32fbc6e7a23eacc
Stanislau Khomich 550503 <stanislaukhomich@gmail.com>
2017-11-25 23:44:23 +0300

    superuser and admin stuff

--- webpages/models.py
+++ webpages/models.py
@@ -6,5 +6,7 @@ class Template(models.Model):
     template_logo = models.CharField(max_length=100)
     template_title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.template_logo + '-' + self.template_title
 #class Template_change(models.Model)
     #template = models.ForeignKey(Template, on_delete=models.CASCADE)
